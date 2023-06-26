f2109caec72fb64eaa8fc7c09dd5912a65c5c216
gil9red <ilya.petrash@inbox.ru>
2023-06-26 13:20:05 +0500

    Refactoring. Using black code style

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'ipetrash'
+__author__ = "ipetrash"
 
 
 import base64
@@ -25,10 +25,15 @@ from flask import Flask, render_template, request, jsonify, send_from_directory
 from flask_socketio import SocketIO
 from engineio.payload import Payload
 
-from third_party.remote_controller__widget_with_webserver import frameless_widget_with_webserver
-from third_party.show_target_icon__behind_cursor import main as show_target_icon__behind_cursor
+from third_party.remote_controller__widget_with_webserver import (
+    frameless_widget_with_webserver,
+)
+from third_party.show_target_icon__behind_cursor import (
+    main as show_target_icon__behind_cursor,
+)
 
 import third_party
+
 sys.path.insert(0, third_party.__path__[0])
 import full_black_screen_close_manual_with_animations
 
@@ -43,14 +48,14 @@ async_mode = None
 DIR = Path(__file__).resolve().parent
 
 PORT_REMOTE_SCREENSHOT = 19999
-URL_REMOTE_SCREENSHOT = 'http://127.0.0.1:%s/command/{}' % PORT_REMOTE_SCREENSHOT
+URL_REMOTE_SCREENSHOT = f"http://127.0.0.1:%s/command/{PORT_REMOTE_SCREENSHOT}"
 
 
 def run_frameless_widget_with_webserver():
     # Запуск виджета-рамки для передачи скриншотов, по умолчанию скрыт
     Process(
         target=frameless_widget_with_webserver.main,
-        args=(PORT_REMOTE_SCREENSHOT, DATA["STREAM_MODE"])
+        args=(PORT_REMOTE_SCREENSHOT, DATA["STREAM_MODE"]),
     ).start()
 
 
@@ -70,12 +75,12 @@ def send_command_frameless_widget_with_webserver(command: str) -> requests.Respo
 pyautogui.FAILSAFE = False
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app, async_mode=async_mode)
 
 
 def get_img_base64(data: bytes) -> str:
-    img_base64 = base64.standard_b64encode(data).decode('utf-8')
+    img_base64 = base64.standard_b64encode(data).decode("utf-8")
     return "data:image/png;base64," + img_base64
 
 
@@ -83,7 +88,7 @@ def get_logger(name=__file__):
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter('[%(asctime)s] %(message)s')
+    formatter = logging.Formatter("[%(asctime)s] %(message)s")
 
     sh = logging.StreamHandler(stream=sys.stdout)
     sh.setFormatter(formatter)
@@ -97,7 +102,7 @@ log = get_logger()
 
 def show_cursor_as_target():
     script_file_name = show_target_icon__behind_cursor.__file__
-    subprocess.Popen([sys.executable, script_file_name, '1000'])
+    subprocess.Popen([sys.executable, script_file_name, "1000"])
 
 
 def full_black_screen():
@@ -117,21 +122,21 @@ def send_about_timer(secs, duration):
         duration = 0
 
     socketio.emit(
-        'about_timer',
-        {'value': secs, 'duration': duration},
+        "about_timer",
+        {"value": secs, "duration": duration},
         broadcast=True,
     )
     # log.info(f'get_timer -> {secs} / {duration}')
 
 
 def send_about_screenshot():
-    log.info(f'send_about_screenshot')
+    log.info(f"send_about_screenshot")
 
     rs = send_command_frameless_widget_with_webserver("SCREENSHOT")
 
     socketio.emit(
-        'about_screenshot',
-        {'img_base64': get_img_base64(rs.content)},
+        "about_screenshot",
+        {"img_base64": get_img_base64(rs.content)},
         broadcast=True,
     )
 
@@ -158,7 +163,7 @@ def timer():
 
             if DT.datetime.now() >= DATA["END_TIME"]:
                 log.info('Timer activate! Press "space"')
-                pyautogui.typewrite(['space'])
+                pyautogui.typewrite(["space"])
                 DATA["END_TIME"] = None
                 continue
 
@@ -189,17 +194,17 @@ thread_screenshot.start()
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/set_timer", methods=['POST'])
+@app.route("/set_timer", methods=["POST"])
 def set_timer():
-    log.info('set_timer')
+    log.info("set_timer")
 
     data = request.get_json()
-    log.info(f'data: {data}')
+    log.info(f"data: {data}")
 
-    secs = int(data['value']) if data['value'] else 0
+    secs = int(data["value"]) if data["value"] else 0
     if secs:
         DATA["END_TIME"] = DT.datetime.now() + DT.timedelta(seconds=secs)
         DATA["DURATION"] = secs
@@ -210,42 +215,42 @@ def set_timer():
     # Понадобится, например, при отмене таймера
     send_about_timer(secs, DATA["DURATION"])
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@socketio.on('connect')
+@socketio.on("connect")
 def on_connect():
-    user_agent = request.headers.get('User-Agent')
+    user_agent = request.headers.get("User-Agent")
 
     secs = get_secs()
     duration = DATA["DURATION"]
 
-    log.info(f'on_connect: {user_agent}\n    secs: {secs}, duration: {duration}')
+    log.info(f"on_connect: {user_agent}\n    secs: {secs}, duration: {duration}")
     send_about_timer(secs, duration)
 
 
-@app.route("/key_click", methods=['POST'])
+@app.route("/key_click", methods=["POST"])
 def key_click():
-    log.info('key_click')
+    log.info("key_click")
 
     data = request.get_json()
-    log.info(f'data: {data}')
+    log.info(f"data: {data}")
 
-    key = data['key']
+    key = data["key"]
 
     # Если включен режим, то перемещаться будет курсор мышки
     # Кнопками удобнее точно целиться курсором мышки
     # Пробелом можно пользоваться
-    if DATA["STREAM_MODE"] and key != 'space':
+    if DATA["STREAM_MODE"] and key != "space":
         relative_x = relative_y = 0
 
-        if key == 'up':
+        if key == "up":
             relative_y = -5
-        elif key == 'down':
+        elif key == "down":
             relative_y = 5
-        elif key == 'left':
+        elif key == "left":
             relative_x = -5
-        elif key == 'right':
+        elif key == "right":
             relative_x = 5
 
         pyautogui.moveRel(xOffset=relative_x, yOffset=relative_y)
@@ -253,112 +258,111 @@ def key_click():
     else:
         pyautogui.typewrite([key])
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route("/mouse_click", methods=['POST'])
+@app.route("/mouse_click", methods=["POST"])
 def mouse_click():
-    log.info('mouse_click')
+    log.info("mouse_click")
 
     data = request.get_json()
-    log.info(f'data: {data}')
+    log.info(f"data: {data}")
 
-    possible_values = 'left', 'right', 'double'
+    possible_values = "left", "right", "double"
 
-    button = data.get('button')
+    button = data.get("button")
     if button not in possible_values:
         text = f'Unsupported mouse button: {button}. Possible values: {", ".join(possible_values)}'
         log.info(text)
-        return jsonify({'text': text})
+        return jsonify({"text": text})
 
-    if button == 'double':
+    if button == "double":
         pyautogui.doubleClick()
     else:
         pyautogui.click(button=button)
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route("/mouse_move", methods=['POST'])
+@app.route("/mouse_move", methods=["POST"])
 def mouse_move():
-    log.info('mouse_move')
+    log.info("mouse_move")
 
     data = request.get_json()
-    log.info(f'data: {data}')
+    log.info(f"data: {data}")
 
-    relative_x = data['relative_x']
-    relative_y = data['relative_y']
+    relative_x = data["relative_x"]
+    relative_y = data["relative_y"]
 
     pyautogui.moveRel(xOffset=relative_x, yOffset=relative_y)
 
     # Если включен режим, то вместе с курсором перемещаться будет окно-рамка
     if DATA["STREAM_MODE"]:
-        send_command_frameless_widget_with_webserver('MOVE_TO_CURSOR')
+        send_command_frameless_widget_with_webserver("MOVE_TO_CURSOR")
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route("/scroll", methods=['POST'])
+@app.route("/scroll", methods=["POST"])
 def scroll():
-    log.info('scroll')
+    log.info("scroll")
 
     data = request.get_json()
-    log.info(f'data: {data}')
+    log.info(f"data: {data}")
 
-    down = data['down']
+    down = data["down"]
 
     value = -200 if down else 200
     pyautogui.scroll(value)
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route("/set_stream_mode", methods=['POST'])
+@app.route("/set_stream_mode", methods=["POST"])
 def set_stream_mode():
-    log.info('set_stream_mode')
+    log.info("set_stream_mode")
 
     data = request.get_json()
-    log.info(f'data: {data}')
+    log.info(f"data: {data}")
 
-    value = data['value']
+    value = data["value"]
     DATA["STREAM_MODE"] = value
 
-    command = 'SHOW' if value else 'HIDE'
+    command = "SHOW" if value else "HIDE"
     send_command_frameless_widget_with_webserver(command)
-    if command == 'SHOW':
-        send_command_frameless_widget_with_webserver('MOVE_TO_CURSOR')
+    if command == "SHOW":
+        send_command_frameless_widget_with_webserver("MOVE_TO_CURSOR")
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route("/show_cursor_as_target", methods=['POST'])
+@app.route("/show_cursor_as_target", methods=["POST"])
 def on_show_cursor_as_target():
-    log.info('show_cursor_as_target')
+    log.info("show_cursor_as_target")
 
     show_cursor_as_target()
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route("/full_black_screen", methods=['POST'])
+@app.route("/full_black_screen", methods=["POST"])
 def on_full_black_screen():
-    log.info('full_black_screen')
+    log.info("full_black_screen")
 
     full_black_screen()
 
-    return jsonify({'text': 'ok'})
+    return jsonify({"text": "ok"})
 
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
     return send_from_directory(
-        os.path.join(app.root_path, 'static/icons'),
-        'favicon.png'
+        os.path.join(app.root_path, "static/icons"), "favicon.png"
     )
 
 
 if __name__ == "__main__":
-    HOST = '0.0.0.0'
+    HOST = "0.0.0.0"
     PORT = 9999
 
     run_frameless_widget_with_webserver()
@@ -370,10 +374,8 @@ if __name__ == "__main__":
 
     # logging.basicConfig(level=logging.DEBUG)
 
-    log.info(f'HTTP server running on http://{"127.0.0.1" if HOST == "0.0.0.0" else HOST}:{PORT}')
-
-    socketio.run(
-        app,
-        host=HOST,
-        port=PORT
+    log.info(
+        f'HTTP server running on http://{"127.0.0.1" if HOST == "0.0.0.0" else HOST}:{PORT}'
     )
+
+    socketio.run(app, host=HOST, port=PORT)
